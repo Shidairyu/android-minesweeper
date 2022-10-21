046a3b687815f30cddd19577fca9da7081664a06
Dario Berther <dario.berther@ost.ch>
2022-10-21 14:13:50 +0200

    BASELINE: Added multiple activities and button setup

---
+++ app/src/main/java/ch/ost/rj/mge/android_minesweeper/activities/LoadGameActivity.kt
@@ -0,0 +1,11 @@
+package ch.ost.rj.mge.android_minesweeper.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_game)
+    }
+}
--- app/src/main/java/ch/ost/rj/mge/android_minesweeper/activities/StartScreenActivity.kt
+++ app/src/main/java/ch/ost/rj/mge/android_minesweeper/activities/StartScreenActivity.kt
@@ -2,7 +2,6 @@ package ch.ost.rj.mge.android_minesweeper.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import ch.ost.rj.mge.android_minesweeper.R
 import ch.ost.rj.mge.android_minesweeper.fragments.StartControlsFragment
 import ch.ost.rj.mge.android_minesweeper.fragments.TitleAndIcon
@@ -10,7 +9,7 @@ import ch.ost.rj.mge.android_minesweeper.fragments.TitleAndIcon
 class StartScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.start_screen)
+        setContentView(R.layout.activity_start_screen)
 
         val titleAndIconFragment = TitleAndIcon();
         val startControlsFragment = StartControlsFragment();
