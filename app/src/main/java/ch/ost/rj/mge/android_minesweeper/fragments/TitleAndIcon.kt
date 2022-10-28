e820f6b26b2b6d87652a9425641b8ea5e6eb15a2
Dario Berther <dario.berther@ost.ch>
2022-10-28 18:30:17 +0200

    Version number on start screen is now set according to version name in build.gradle file

--- app/src/main/java/ch/ost/rj/mge/android_minesweeper/fragments/TitleAndIcon.kt
+++ app/src/main/java/ch/ost/rj/mge/android_minesweeper/fragments/TitleAndIcon.kt
@@ -1,20 +1,27 @@
 package ch.ost.rj.mge.android_minesweeper.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
+import ch.ost.rj.mge.android_minesweeper.BuildConfig
 import ch.ost.rj.mge.android_minesweeper.R
-import ch.ost.rj.mge.android_minesweeper.activities.HelpActivity
 
 class TitleAndIcon : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val versionText = view.findViewById<TextView>(R.id.version)
+        versionText.text = buildString {
+            append("Version ")
+            append(BuildConfig.VERSION_NAME)
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_title_and_icon, container, false)
     }
 }
