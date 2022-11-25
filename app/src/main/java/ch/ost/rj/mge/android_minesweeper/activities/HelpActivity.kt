fff2dd70a4b2116ebff67e19e8580dab28be1215
Dario Berther <dario.berther@ost.ch>
2022-11-25 14:02:38 +0100

    Removed TODO

--- app/src/main/java/ch/ost/rj/mge/android_minesweeper/activities/HelpActivity.kt
+++ app/src/main/java/ch/ost/rj/mge/android_minesweeper/activities/HelpActivity.kt
@@ -6,7 +6,6 @@ import ch.ost.rj.mge.android_minesweeper.R
 import ch.ost.rj.mge.android_minesweeper.adapter.HelpViewPagerAdapter
 import ch.ost.rj.mge.android_minesweeper.databinding.ActivityHelpBinding
 
-// TODO Finish Helper pages with descriptions and title
 class HelpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
